23ee337c185a5453c35afbb75b64e201f66b5092
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-29 21:21:36 +0800

    Type select at home added

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,14 @@ import { Shout } from "./Shout";
 
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+import { useState } from 'react';
+
 export function Home(props){
 
+    const [type, setType] = useState("createdAt")
+
     const shoutsRef = firestore.collection("shouts");
-    const query = shoutsRef.orderBy("createdAt", "desc");
+    const query = shoutsRef.orderBy(type, "desc");
 
     const [shouts] = useCollectionData(query, {idField:'id'});
 
@@ -17,7 +21,14 @@ export function Home(props){
     }
 
     return (<div id="homepage" className="hide" ref={props.homePageRef}>
-        <h2>Latest Shouts</h2>
+        <div id="header">
+            <h2>Latest Shouts</h2>
+            <select onChange={e => setType(e.target.value)} value={type}>
+                <option value="createdAt">Lastest</option>
+                <option value="likeCount">Most Likes</option>
+                <option value="commentCount">Most Comments</option>
+            </select>
+        </div>
         <div id="shouts">
             {shouts && shouts.map(shout => <Shout imageClick={openViewProfile} setViewUid={props.setViewUid} setViews={props.setViewVariables} key={shout.id} shoutData={shout}/>)}
         </div>
